27340a9503ab45357f3a8f763cf32e302c25aca7
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-10-20 14:48:22 +0200

    feat: add dropdown for the days

--- src/script.js
+++ src/script.js
@@ -4,3 +4,9 @@ const unitsButton = document.getElementById("units");
 unitsButton.addEventListener("click", () => {
   dropdownUnits.classList.toggle("hidden");
 });
+
+const daysButton = document.getElementById("days-btn");
+const dropdownDays = document.getElementById("days-model");
+daysButton.addEventListener("click", () => {
+  dropdownDays.classList.toggle("hidden");
+});
